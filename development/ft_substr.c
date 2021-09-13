61a12d6e4c2dfc07e48aa0ec69bf224dcc3f6cfd
Egydio Pacheco <egydio.neto@usp.br>
2021-09-13 14:27:10 -0300

    fix wrong casting

--- development/ft_substr.c
+++ development/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: epacheco <epacheco@student.42sp.org.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 13:36:23 by epacheco          #+#    #+#             */
-/*   Updated: 2021/09/11 13:36:25 by epacheco         ###   ########.fr       */
+/*   Updated: 2021/09/13 14:26:56 by epacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	t;
 	size_t	v;
 
-	substr = malloc(sizeof(char *) * (len + 1));
+	substr = malloc(sizeof(char) * (len + 1));
 	if (!substr)
 		return (NULL);
 	t = 0;

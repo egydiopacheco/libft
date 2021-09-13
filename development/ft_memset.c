1f331b0b787f80b419a331a79b0e9abe63488568
Egydio Pacheco <egydio.neto@usp.br>
2021-09-13 12:56:20 -0300

    Remove incorrect checking

--- development/ft_memset.c
+++ development/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: epacheco <epacheco@student.42sp.org.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 13:35:05 by epacheco          #+#    #+#             */
-/*   Updated: 2021/09/11 13:35:07 by epacheco         ###   ########.fr       */
+/*   Updated: 2021/09/13 12:55:22 by epacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memset(void *s, int c, size_t n)
 	char	*p;
 	size_t	j;
 
-	if (!n)
-		return (NULL);
 	p = (char *) s;
 	j = 0;
 	while (j < n)

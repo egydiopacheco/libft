d797a8d2d4fc79d80709bf10eaf3cdf17f3077e3
Egydio Pacheco <egydio.neto@usp.br>
2021-09-13 13:38:07 -0300

    add verification and solve malloc size

--- development/ft_calloc.c
+++ development/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: epacheco <epacheco@student.42sp.org.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 13:33:43 by epacheco          #+#    #+#             */
-/*   Updated: 2021/09/11 13:33:47 by epacheco         ###   ########.fr       */
+/*   Updated: 2021/09/13 13:37:44 by epacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	ns = nmemb * size;
 	if (ns > 2147483647)
 		return (NULL);
-	ptr = (void *)malloc(sizeof(ns));
-	ft_bzero(ptr, ns);
+	ptr = (void *)malloc(ns);
 	if (!ptr)
 		return (NULL);
+	ft_bzero(ptr, ns);
 	return (ptr);
 }

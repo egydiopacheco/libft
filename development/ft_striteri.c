02c26b83902926f6f15ed3e4c71d3cb9676e03df
Egydio Pacheco <egydio.neto@usp.br>
2021-09-14 10:40:34 -0300

    add protection

--- development/ft_striteri.c
+++ development/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: epacheco <epacheco@student.42sp.org.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 13:35:44 by epacheco          #+#    #+#             */
-/*   Updated: 2021/09/11 13:35:46 by epacheco         ###   ########.fr       */
+/*   Updated: 2021/09/14 10:40:12 by epacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	index;
 
+	if (!s || !f)
+		return ;
 	index = 0;
 	while (*s && s)
 		f(index++, s++);
